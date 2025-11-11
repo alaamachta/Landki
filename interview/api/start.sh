8412a6986aa6270109fc9677725382f0394bdda9
Alaa Mashta <alaamachta@gmail.com>
2025-11-11 20:31:18 +0100

    T00

--- interview/api/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Load environment variables from .env file
-set -a
-source /var/www/landki/interview/.env
-set +a
-
-# Start the server
-exec node /var/www/landki/interview/api/dist/server.js
